e9acb6ad830b6cee90454a672aac11c353b5e628
AJG91 <albertojg12@gmail.com>
2023-01-23 10:29:46 -0500

    Added plotting functions.

--- libraries/__init__.py
+++ libraries/__init__.py
@@ -3,3 +3,4 @@ from .timing import CheckTime
 # from .neural_network import build_loss, corr_matrix, initialize, train, predict
 from .utils import check_model_exists, get_training_data, construct_training_sets
 from .neural_network import build_loss, correlation_matrix, NeuralNetwork
+from .plots import plot_counts, plot_prediction
